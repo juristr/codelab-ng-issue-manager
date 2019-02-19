72e656b6d31aa43cc87e1297c28deb8d0eb3d712
Juri <juri.strumpflohner@gmail.com>
2019-02-19 23:04:24 +0100

    add issue-list routing module

---
+++ src/app/issue-list/issue-list.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { IssueListRoutingModule } from './issue-list-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    IssueListRoutingModule
+  ]
+})
+export class IssueListModule { }
